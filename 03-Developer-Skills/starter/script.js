8cd751b1c43fc95525512829123d5d16140f880f
Pushpendra Uikey <pushpendrauikey963@gmail.com>
2024-05-13 12:56:36 +0530

    Started my learning of Javascript

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) console.log('Twenty Three');
+
+console.log();
+
+// One most important thing of the coding is writing the psuedo-code, how
+// will it look like, and then make the code work, by doing different parts by diff parts
+
+// break the problems into sub-problems,
+// understand the problem,
